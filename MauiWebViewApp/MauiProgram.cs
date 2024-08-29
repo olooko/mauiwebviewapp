c64c07ad3717baa375a5c2b5d083a299a76ef6ff
김호섭 <hskim@beaverworksinc.com>
2024-08-29 16:10:48 +0900

    fullscreen

--- MauiWebViewApp/MauiProgram.cs
+++ MauiWebViewApp/MauiProgram.cs
@@ -1,9 +1,35 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using System.Runtime.InteropServices;
+using Windows.Graphics;
+#endif 
 
 namespace MauiWebViewApp
 {
     public static class MauiProgram
     {
+#if WINDOWS
+        public enum DWMWINDOWATTRIBUTE
+        {
+            DWMWA_WINDOW_CORNER_PREFERENCE = 33
+        }
+
+        public enum DWM_WINDOW_CORNER_PREFERENCE
+        {
+            DWMWCP_DEFAULT = 0,
+            DWMWCP_DONOTROUND = 1,
+            DWMWCP_ROUND = 2,
+            DWMWCP_ROUNDSMALL = 3
+        }
+
+        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
+        internal static extern void DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE attribute, ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute, uint cbAttribute);
+#endif
+
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -15,8 +41,50 @@ namespace MauiWebViewApp
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(wndLifeCycleBuilder =>
+                {
+                    wndLifeCycleBuilder.OnWindowCreated(window =>
+                    {
+                        try
+                        {
+                            window.ExtendsContentIntoTitleBar = false;
+
+                            IntPtr handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                            WindowId windowId = Win32Interop.GetWindowIdFromWindow(handle);
+                            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+
+                            if (appWindow.Presenter is OverlappedPresenter p)
+                            {
+                                p.IsResizable = false;
+                                p.SetBorderAndTitleBar(false, false);
+                            }
+
+                            int x = 0;
+                            int y = 0;
+                            int width = (int)DeviceDisplay.Current.MainDisplayInfo.Width;
+                            int height = (int)DeviceDisplay.Current.MainDisplayInfo.Height;
+
+                            appWindow.MoveAndResize(new RectInt32(x, y, width, height));
+
+                            var attribute = DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE;
+                            var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_DONOTROUND;
+
+                            DwmSetWindowAttribute(handle, attribute, ref preference, sizeof(uint));
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    });
+                });
+            });
+#endif
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
--- MauiWebViewApp/Platforms/Android/MainActivity.cs
+++ MauiWebViewApp/Platforms/Android/MainActivity.cs
@@ -1,11 +1,50 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace MauiWebViewApp
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.NoSplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Platform.Init(this, savedInstanceState);
+
+            DeviceDisplay.Current.KeepScreenOn = true;
+
+            if (Window != null)
+            {
+                SetWindow(Window);
+            }
+        }
+
+        static void SetWindow(Android.Views.Window window)
+        {
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+            {
+#pragma warning disable CA1416
+                IWindowInsetsController? wicController = window.InsetsController;
+
+                window.SetDecorFitsSystemWindows(false);
+                window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
+
+                if (wicController != null)
+                {
+                    wicController.Hide(WindowInsets.Type.Ime());
+                    wicController.Hide(WindowInsets.Type.NavigationBars());
+                }
+#pragma warning restore CA1416
+            }
+            else
+            {
+#pragma warning disable CS0618
+                window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
+                window.DecorView.SystemUiVisibility = (StatusBarVisibility)(SystemUiFlags.Fullscreen | SystemUiFlags.HideNavigation | SystemUiFlags.Immersive | SystemUiFlags.ImmersiveSticky | SystemUiFlags.LayoutHideNavigation | SystemUiFlags.LayoutStable | SystemUiFlags.LowProfile);
+#pragma warning restore CS0618
+            }
+        }
     }
 }
